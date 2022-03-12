b1a320047e12687508540bae1e0d0d7b1999e6a6
NamSPro <donguyenhainam2812@gmail.com>
2022-03-13 03:43:16 +0900

    add KR migration sheet to /sheet

--- commands/sheet.js
+++ commands/sheet.js
@@ -2,12 +2,29 @@
 // Returns a link to the general KGA spreadsheet
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('sheet')
-		.setDescription('Get a link to the info spreadsheet'),
+		.setDescription('Get a link to various spreadsheets'),
 	async execute(interaction) {
-		await interaction.reply('<https://docs.google.com/spreadsheets/d/1y6EBqUxntzWsa0ZoBMEbHXwIsLCpMNChos3tMyYogco/edit#gid=300669334>');
+		await interaction.deferReply();
+
+		const buttonRow = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setStyle('LINK')
+					.setLabel('KGA Main Infos')
+					.setURL('https://docs.google.com/spreadsheets/d/1y6EBqUxntzWsa0ZoBMEbHXwIsLCpMNChos3tMyYogco/edit#gid=300669334')
+			)
+			.addComponents(
+				new MessageButton()
+					.setStyle('LINK')
+					.setLabel('KR to KGA Infos')
+					.setURL('https://docs.google.com/spreadsheets/d/16t0cvTTSS_O9w0tm8cSPuShkvN__qQ1pcO2iNLV-Mx0/edit#gid=656576501')
+			);
+		
+		await interaction.editReply({ content: 'Below are some useful spreadsheets:', components: [buttonRow] });
 	},
 };
